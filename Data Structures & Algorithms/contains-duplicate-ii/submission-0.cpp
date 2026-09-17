bbaf31a85dabdc9981b80b86a7648fdd19e57162
hellolwl <hellolwl@gmail.com>
2026-09-17 00:12:36 -0500

    Add: contains-duplicate-ii - submission-0

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> last_index;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (last_index.count(nums[i]) && (i - last_index[nums[i]]) <= k) {
+                return true;
+            }
+
+            last_index[nums[i]] = i;
+        }
+
+        return false;
+    }
+};
